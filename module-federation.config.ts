b8c037ce014c4889c08bf9561b28c66818c45349
007qr <aayushpatil558321@gmail.com>
2025-05-18 15:36:51 +0530

    pushed

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -4,10 +4,10 @@ export default federation({
     name: 'stopper',
     filename: 'remoteEntry.js',
     exposes: {
-        './Descriptor': './src/components/descriptor/Descriptor.tsx',
-        './DescriptorFlowProvider': './src/components/descriptor/DescriptorFlowProvider.tsx',
-        './Button': './src/ui/base/Button.tsx',
-        './AIWidget': './src/components/AiResponse/AIWidget.tsx'
+        './Descriptor': '~/src/components/descriptor/Descriptor.tsx',
+        './DescriptorFlowProvider': '~/src/components/descriptor/DescriptorFlowProvider.tsx',
+        './Button': '~/src/ui/base/Button.tsx',
+        './AIWidget': '~/src/components/AiResponse/AIWidget.tsx'
         // './ListDescriptor': './src/components/descriptor/screens/ListDescriptor.tsx',
     },
     shared: ['solid-js'],
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
-import tailwindcss from "@tailwindcss/vite";
-import federation from "./module-federation.config";
-import path from "path";
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import tailwindcss from '@tailwindcss/vite'
+// import federation from './module-federation.config'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [federation, tailwindcss(), solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: "esnext",
-    minify: "esbuild",
-    cssCodeSplit: false,
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        minifyInternalExports: false,
-      },
+    plugins: [tailwindcss(), solidPlugin()],
+    server: {
+        port: 3000,
     },
-  },
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "./src"),
+    build: {
+        target: 'esnext',
+        minify: 'esbuild',
+        cssCodeSplit: false,
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                minifyInternalExports: false,
+            },
+        },
     },
-  },
-});
+    resolve: {
+        alias: {
+            '~': path.resolve(__dirname, './src'),
+        },
+    },
+})
